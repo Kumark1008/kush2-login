34fa086b9be3ad7cf599a96d5ee7b3d872404d83
Kumark1008 <kumark.aksf08@gmail.com>
2020-05-06 19:25:52 +0530

    api key updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,14 +16,14 @@ const config = new AuthServiceConfig([
     id: GoogleLoginProvider.PROVIDER_ID,
     provider: new GoogleLoginProvider('528961187921-ld24b25466u4t2lacn9r35asg000lfis.apps.googleusercontent.com')
   },
-  // {
-  //   id: FacebookLoginProvider.PROVIDER_ID,
-  //   provider: new FacebookLoginProvider('561602290896109')
-  // },
-  // {
-  //   id: LinkedInLoginProvider.PROVIDER_ID,
-  //   provider: new LinkedInLoginProvider("78iqy5cu2e1fgr")
-  // }
+   {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('231730084593191')
+   },
+ {
+   id: LinkedInLoginProvider.PROVIDER_ID,
+    provider: new LinkedInLoginProvider("86se1lho1mjs6n")
+  }
 ]);
 
 export function provideConfig() {
